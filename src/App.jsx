be4930c8111419fd9ad0ea37b53e29f34d6da673
Aayus <godlord@AayushPathak>
2024-02-17 16:11:46 +0530

    Added tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import './index.css';
+import FirstPage from './pages/firstpage/first';
+function App() {
+  return (
+    <div className="container mx-auto">
+      <FirstPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/firstpage/first.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FirstPage = () => {
+    return (
+        <div>
+            <div className="text-darkgreen text-4xl">
+                <h1 className="leading-tight">
+                    Empowering<br />
+                    Futures Through<br />Education
+                </h1>
+            </div>
+        </div>
+    );
+};
+
+export default FirstPage;
