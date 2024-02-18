24867c8c07741e91aad572be117a2a811d474bd1
Aayus <godlord@AayushPathak>
2024-02-18 11:45:33 +0530

    Added 4th page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './index.css';
 import FirstPage from './pages/firstpage/first';
 import SecondPage from './pages/secondpage/second';
 import ThirdPage from './pages/thirdpage/thrid';
+import FourthPage from './pages/fouthpage/fourth';
 function App() {
   return (
     <div className="container mx-auto">
       <FirstPage />
       <SecondPage/>
       <ThirdPage/>
+      <FourthPage/>
     </div>
   );
 }
--- src/pages/secondpage/second.jsx
+++ src/pages/secondpage/second.jsx
@@ -4,10 +4,10 @@ import Image2 from "/home/godlord/intern/task/task-page/src/assets/pic2.png";
 
 const SecondPage = () => {
     return (    
-        <div className="second bg-[#fff8e8] min-h-screen relative">
+        <div className="second bg-white min-h-screen relative">
             <img
                 src={Image2}
-                className="absolute left-20 top-0  w-1/3 h-3/4 object-cover transition-all duration-1000 hover:rounded-r-full"
+                className="absolute left-20 top-10  w-1/3 h-3/4 object-cover transition-all duration-1000 rounded-3xl hover:rounded-r-full"
             />
             <div className="p-10 text-yellow-500 font-bold text-center absolute" style={{ top: "10%", left: "54.2%", transform: "translate(-15%, -30%)" }}>OUR STORY</div>
             <div className="p-3 py-2 font-bold mt-10 text-green-900 text-4xl absolute" style={{ top: "25%", left: "60%", transform: "translate(-15%, -30%)" }}>
